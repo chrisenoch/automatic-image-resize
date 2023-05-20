b7fb21bcd49ff711969eae99a133dd8e0e2f1eb5
chrisenoch <christopher.enoch@yahoo.co.uk>
2023-05-20 16:09:34 +0200

    If an image has a width suffix that is specified in the widths array, it it is overwritten. If no image exists with a width suffix specified in the widths array, it is created

--- js/resize-images.js
+++ js/resize-images.js
@@ -3,24 +3,50 @@ const Jimp = require('jimp');
 const fs = require('fs');
 const path = require('path');
 
-function walkDir(dir, callback) {
-    fs.readdirSync(dir).forEach(f => {
-        let dirPath = path.join(dir, f);
-        let isDirectory = fs.statSync(dirPath).isDirectory();
-        isDirectory ?
-            walkDir(dirPath, callback) : callback(dirPath);
-    });
-};
-
 let images = [];
-walkDir('../img', function (filePath) {
+walkDir('C:\\IT\\Javascript\\my_projects\\auto-resize\\img', function (filePath) {
     images.push(filePath);
 });
+
+// let widths = [400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1400, 1600, 1800, 2000, 2100, 2200];
+let widths = [2700];
+let quality = 75;
+let count = 0;
+const pathSeparator = path.sep;
+images.forEach((image) => {
+    let imageStart = image.substring(0, image.lastIndexOf('.'));
+    for (let width of widths) {
+        count++
+        imageStart = path.normalize(imageStart);
+        let currentName = imageStart.substring(imageStart.lastIndexOf(pathSeparator) + 1);
+
+        //check if already has a width. If so, skip
+        let possibleExistingWidth = currentName.substring(currentName.lastIndexOf('-') + 1);
+        if (checkIfNumber(possibleExistingWidth)) {
+            continue;
+        }
+
+        let newName = currentName + '-' + width;
+        console.log("new image name: " + newName);
+        //console.log(count);
+        resizeImage(image, newName, quality, width, false);
+    }
+});
+
+function checkIfNumber(value) {
+    if (typeof value === 'string') {
+        value = +value;
+    }
+    if (typeof value === 'number' && !isNaN(value)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 function resizeImage(imagePath, newImageName, quality, width, height) {
     const pathSeparator = path.sep;
     imagePath = path.normalize(imagePath);
-
     const imageDirectoryParts = imagePath.split(pathSeparator);
     imageDirectoryParts.pop(); // get rid of image file name
     const imageLocation = imageDirectoryParts.join(pathSeparator) + pathSeparator;
@@ -41,6 +67,7 @@ function resizeImage(imagePath, newImageName, quality, width, height) {
             image.quality(quality);
             const fileExt = image.getExtension();
             image.write(imageLocation + newImageName + '.' + fileExt);
+            console.log(imageLocation + newImageName + '.' + fileExt);
         })
         .catch(err => {
             console.error(err)
@@ -48,18 +75,12 @@ function resizeImage(imagePath, newImageName, quality, width, height) {
 
 }
 
-let widths = [400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1400, 1600, 1800, 2000, 2100, 2200];
-let quality = 75;
-let count = 0;
-const pathSeparator = path.sep;
-images.forEach((image) => {
-    let imageStart = image.substring(0, image.lastIndexOf('.'));
-    widths.forEach((width) => {
-        count++
-        imageStart = path.normalize(imageStart);
-        let currentName = imageStart.substring(imageStart.lastIndexOf(pathSeparator) + 1);
-        let newName = currentName + '-' + width;
-        console.log(count);
-        resizeImage(image, newName, quality, width, false);
-    })
-});
+function walkDir(dir, callback) {
+    fs.readdirSync(dir).forEach(f => {
+        let dirPath = path.join(dir, f);
+        let isDirectory = fs.statSync(dirPath).isDirectory();
+        isDirectory ?
+            walkDir(dirPath, callback) : callback(dirPath);
+    });
+};
+
